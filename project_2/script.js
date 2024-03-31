dc7d3434e0086a5a6792c8f45b1b851edc98326b
bluskova <barbora.luskova@gmail.com>
2024-03-31 22:03:57 +0200

    wip

---
+++ project_2/script.js
@@ -0,0 +1,21 @@
+function replaceClass(element, oldClass, newClass) {
+  element.classList.remove(oldClass);
+  element.classList.add(newClass);
+}
+
+const menuIcon = document.querySelector("header .menu-icon i");
+const navigation = document.querySelector("header nav");
+
+let hiddenMenu = true;
+
+menuIcon.addEventListener("click", () => {
+  if (hiddenMenu) {
+    navigation.style.display = "block";
+    replaceClass(menuIcon, "fa-bars", "fa-xmark");
+    hiddenMenu = false;
+  } else {
+    navigation.style.display = "none";
+    replaceClass(menuIcon, "fa-xmark", "fa-bars");
+    hiddenMenu = true;
+  }
+});
