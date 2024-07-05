e6324b2f6870804ae44820e2249c5cadd78acdac
bluskova <barbora.luskova@gmail.com>
2024-07-05 22:18:54 +0200

    wip

--- project_2/script.js
+++ project_2/script.js
@@ -1,53 +1,104 @@
 // select from html
-const menuIcon = document.querySelector("header .menu-icon-container i");
+const menuIcon = document.getElementById("menu-icon");
 const navigation = document.querySelector("header nav");
-const navigationLinks = document.querySelectorAll("header nav li");
 const heroSection = document.querySelector(".hero-section");
+const dots = document.querySelectorAll(".dot");
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav li a");
 
 // function which replace the hamburger and cross icon
-function replaceClass(element, oldClass, newClass) {
+const replaceClass = (element, oldClass, newClass) => {
   element.classList.remove(oldClass);
   element.classList.add(newClass);
-}
+};
+
+// function which open navigation
+const openNav = () => {
+  navigation.style.display = "block";
+  replaceClass(menuIcon, "fa-bars", "fa-xmark");
+  hiddenNav = false;
+};
+
+// function which close navigation
+const closeNav = () => {
+  navigation.style.display = "none";
+  replaceClass(menuIcon, "fa-xmark", "fa-bars");
+  hiddenNav = true;
+};
 
 // open and close navigation
 let hiddenNav = true;
 menuIcon.addEventListener("click", () => {
   if (hiddenNav) {
-    navigation.style.display = "block";
-    replaceClass(menuIcon, "fa-bars", "fa-xmark");
-    hiddenNav = false;
+    openNav();
   } else {
-    navigation.style.display = "none";
-    replaceClass(menuIcon, "fa-xmark", "fa-bars");
-    hiddenNav = true;
+    closeNav();
   }
 });
 
-// close the navigation after click any link
-navigationLinks.forEach((navLink) =>
-  navLink.addEventListener("click", () => {
-    navigation.style.display = "none";
-    replaceClass(menuIcon, "fa-xmark", "fa-bars");
-    hiddenNav = true;
-  })
-);
+// // close the navigation after click any link
+// navLinks.forEach((navLink) =>
+//   navLink.addEventListener("click", () => {
+//     closeNav();
+//   })
+// );
+
+// close the navigation after click anywhere (include 'navLinks', exclude 'menuIcon')
+document.body.addEventListener("click", (event) => {
+  if (!hiddenNav && event.target.id !== "menu-icon") {
+    closeNav();
+  }
+});
 
 // change hero image every 3 sec
 const imageSources = [
-  "img/hero-img/chaos-soccer-gear-Cjfl8r_eYxY-unsplash.jpg",
-  "img/hero-img/omar-ram-kHhLJMRpRDI-unsplash.jpg",
-  "img/hero-img/vikram-tkv-JO19K0HDDXI-unsplash.jpg",
-  "img/hero-img/peter-glaser-UVWULib2OHM-unsplash.jpg",
+  "./img/hero-img/peter-glaser-UVWULib2OHM-unsplash.jpg",
+  "./img/hero-img/chaos-soccer-gear-Cjfl8r_eYxY-unsplash.jpg",
+  // "./img/hero-img/omar-ram-kHhLJMRpRDI-unsplash.jpg",
+  "./img/hero-img/vikram-tkv-JO19K0HDDXI-unsplash.jpg",
 ];
 
-let index = 0;
+let imageIndex = 0;
 const changePhotos = () => {
-  index = index % imageSources.length;
-  heroSection.style.backgroundImage = `url(${imageSources[index]})`;
-  index++;
+  dots[imageIndex].classList.remove("active");
+  imageIndex = (imageIndex + 1) % imageSources.length;
+  dots[imageIndex].classList.add("active");
+  heroSection.style.backgroundImage = `url(${imageSources[imageIndex]})`;
 };
 
-const timerID = setInterval(changePhotos, 3000);
-
+const timerID = setInterval(changePhotos, 5000);
 // clearInterval(timerID);
+
+// active navbar links on scroll
+// window.onscroll = () => {
+//   sections.forEach((oneSection) => {
+//     const top = window.scrollY;
+//     const offset = oneSection.offsetTop - 150;
+//     const height = oneSection.offsetHeight;
+//     const id = oneSection.getAttribute("id");
+//     if (top >= offset && top < offset + height) {
+//       navLinks.forEach((oneLink) => {
+//         oneLink.classList.remove("active");
+//       });
+//       document
+//         .querySelector("header nav li a[href*=" + id + "]")
+//         .classList.add("active");
+//     }
+//   });
+// };
+
+// active navbar links on scroll
+addEventListener("scroll", () => {
+  sections.forEach((oneSection, index) => {
+    const top = window.scrollY;
+    const offset = oneSection.offsetTop - 150;
+    const height = oneSection.offsetHeight;
+    // const id = oneSection.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((oneLink) => {
+        oneLink.classList.remove("active");
+      });
+      navLinks[index].classList.add("active");
+    }
+  });
+});
