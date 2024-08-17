b1add6f4c0749a9b8673cd005570efd0fb8ef8f1
bluskova <barbora.luskova@gmail.com>
2024-08-17 17:41:17 +0200

    form-login

---
+++ final-project-3/js/form-login.js
@@ -0,0 +1,4 @@
+const name = document.getElementById("username");
+const psw1 = document.getElementById("psw-1");
+const psw2 = document.getElementById("psw-2");
+const pswText = document.getElementById("psw-text");
